83774f8c2de1f6aa7610607022cb1447db5a21a8
ssoo174 <itssoo174@gmail.com>
2024-01-06 17:08:17 +0900

    수정!수정

--- app/src/main/java/com/example/cocktail_week2/MainActivity.kt
+++ app/src/main/java/com/example/cocktail_week2/MainActivity.kt
@@ -53,11 +53,28 @@ class MainActivity : AppCompatActivity() {
                     return@setOnClickListener
                 }
             }
-            // 서버 요청 없이 로그인 성공으로 가정하고 LogActivity로 바로 이동
-            Toast.makeText(applicationContext, "로그인 성공", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this@MainActivity, LogActivity::class.java)
-            intent.putExtra("id", binding.inputID.text.toString())
-            startActivity(intent)
+            val loginUser = LoginModel(binding.inputID.text.toString(), binding.inputPw.text.toString())
+            api.login(loginUser).enqueue(object: Callback<LoginResult>{
+                override fun onResponse(call: Call<LoginResult>, response: Response<LoginResult>) {
+                    val user_uid = response.body()?.UID ?: return
+                    if(user_uid != -1) {
+                        Toast.makeText(applicationContext, "로그인 성공", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this@MainActivity, SecondActivity::class.java)
+                        intent.putExtra("id", binding.inputID.text.toString())
+                        startActivity(intent)
+
+                        Log.d("test1", user_uid.toString())
+                    }
+                    else{
+                        Toast.makeText(applicationContext, "로그인 실패, 아이디 또는 비밀번호를 확인해주세요.", Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+
+                override fun onFailure(call: Call<LoginResult>, t: Throwable) {
+                    Log.d("test2", t.message.toString())
+                }
+            })
         }
         binding.allUserButton.setOnClickListener {
             api.allUser().enqueue(object:Callback<ArrayList<User>>{
